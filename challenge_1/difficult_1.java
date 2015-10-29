327f002825e5805752a908de0fba9dd8f457d74c
jasonou <jasonou95@gmail.com>
2015-10-29 15:19:34 -0700

    Challenge 1 easy and difficult complete, comments added

--- challenge_1/difficult_1.java
+++ challenge_1/difficult_1.java
@@ -1,7 +1,53 @@
+/**
+ * Reddit [Difficult] Challenge #1
+ * https://www.reddit.com/r/dailyprogrammer/comments/pii6j/difficult_challenge_1/
+ *
+ * we all know the classic "guessing game" with higher or lower prompts. 
+ * Lets do a role reversal; you create a program that will guess numbers between 1-100, 
+ * and respond appropriately based on whether users say that the number is too high or too low
+ */
+ 
+import java.util.Scanner;
+
 class Difficult1 {
 	
-	public static void main(String[] args) {
+	public static void GuessIt(int low, int high, int guess) {
 		
+		char useropt;
+		// Checks for user input if the user inputs (y), (l), or (h)
+		for (;;) {
+			Scanner scan = new Scanner(System.in);
+			System.out.printf("Is your number %d? (y)es, (l)ower, (h)igher : ", guess);
+			// gets user input chars
+			useropt = scan.next().charAt(0);
+			// checks for valid input
+			if (useropt != 'y' && useropt != 'l' && useropt != 'h') {
+				System.out.println("Invalid Input, Usage: [y | l | h].");
+				continue;
+			}
+			// if valid input, exits infinite loop
+			break;
+		}
+		
+		// if user says y, then exit
+		if (useropt == 'y') {
+			System.out.printf("Your secret number is %d!\n", guess);
+			return;
+		} else if (useropt == 'l') {
+			// l, creates lower bound
+			high = guess - 1;
+		} else {
+			// h, creates upper bound
+			low = guess + 1;
+		}
+		// Recursively calls itself until guess is correct
+		GuessIt(low, high, (high + low)/2);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Think of a number from 1 - 100!");
+		// initial guess is 50 from the range of 1 - 100
+		GuessIt(1, 100, 50);
 	}
 	
 }
--- challenge_1/easy_1.java
+++ challenge_1/easy_1.java
@@ -7,21 +7,36 @@
  */
  
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 class Easy1 {
 	
-	public static void main(String[] args) {
+	// Asks the user for their info and prints it out
+	public static void AskUser() {
 		// In java you strings and ints must be scanned in separately
 		Scanner askString = new Scanner(System.in);
-		Scanner askInt = new Scanner(System.in);
 		
 		// Asks user their name and stores it
 		System.out.print("What is your name? : ");
 		String name = askString.nextLine();
 		
 		// Asks user their age and stores it
-		System.out.print("What is your age? : ");
-		int age = askInt.nextInt();
+		int age = 0;
+		for(;;) {
+			// Int scanner must be separated from String
+			Scanner askInt = new Scanner(System.in);
+			// Checks if the user inputs a string or int
+			try {
+				System.out.print("What is your age? : ");
+				age = askInt.nextInt();			
+			} catch (InputMismatchException e) {
+				// Scanning in a string outputs an error
+				System.out.println("Invalid Input, Usage: [Integer].");
+				continue;
+			}
+			// if user inputs a successful int, it breaks out the loop
+			break;
+		}
 		
 		// Asks user their Reddit username and stores it
 		System.out.print("What is your Reddit username? : ");
@@ -29,6 +44,11 @@ class Easy1 {
 		
 		// Prints out their info
 		System.out.printf("\nYour name is %s, you are %d years old, and your Reddit username is %s.\n", name, age, redditu);
+	}
+	
+	public static void main(String[] args) {
+		// Calls function AskUser()
+		AskUser();
 	}
 	
 }
